5fc5a7549143b24523719aa51fff559b9dd63d56
Маркер <satonin18@mail.ru>
2018-02-02 19:07:54 +0500

    Global !!!

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,10 @@ public class GitTest {
     public static void main(String args[]){
         System.out.println("Hello");
         System.out.println("Fuck You !!!");
+        System.out.println("Global !!!");
+        System.out.println("Global !!!");
+        System.out.println("Global !!!");
+        System.out.println("Global !!!");
+
     }
 }
