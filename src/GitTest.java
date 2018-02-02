6048e39371430f89703cce835bea596a6e835707
Маркер <satonin18@mail.ru>
2018-02-02 19:20:48 +0500

    NEW 19:20

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,11 +1,5 @@
 public class GitTest {
     public static void main(String args[]){
-        System.out.println("Hello");
-        System.out.println("Fuck You !!!");
-        System.out.println("Global !!!");
-        System.out.println("Global !!!");
-        System.out.println("Global !!!");
-        System.out.println("Global !!!");
-
+        System.out.println("NEW Fuck You !!!");
     }
 }
